af499e44f2b335377067075fedf8d900e88610f4
Vince Parker <vp00170@surrey.ac.uk>
2023-07-12 16:11:15 +0100

    Added some tests to the home page

--- homeapp/tests.py
+++ homeapp/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+
+    """Test whether the Coffee Club application homepage"""
+    def setUp(self):
+        return
+
+    def test_homepage(self):
+        response = self.client.get('/home/')
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, 'This is My Home Page')
+        self.assertContains(response, 'This is my footer')
